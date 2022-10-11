5a1ece640e290b6004a7bc9e2e576a6ff86ab573
Denis Khmelnov <denishmelnov@gmail.com>
2022-10-11 13:14:37 +0500

    feat: implement authorization

--- project/dao/main.py
+++ project/dao/main.py
@@ -2,6 +2,7 @@ from werkzeug.exceptions import NotFound
 
 from project.dao.base import BaseDAO
 from project.models import Genre, Director, Movie, User
+from project.tools.security import generate_password_hash
 from sqlalchemy import desc
 
 
@@ -27,5 +28,38 @@ class MoviesDAO(BaseDAO[Movie]):
                 return []
         return stmt.all()
 
+
 class UsersDAO(BaseDAO[User]):
     __model__ = User
+
+    def create(self, login, password):
+        try:
+            self._db_session.add(
+                User(
+                    email=login,
+                    password=generate_password_hash(password)
+                )
+            )
+            self._db_session.commit()
+            print("Пользователь добавлен")
+        except Exception as e:
+            print(e)
+            self._db_session.rollback()
+
+    def get_user_by_login(self, login):
+        try:
+            stmt = self._db_session.query(self.__model__).filter(self.__model__.email == login).one()
+            return stmt
+        except Exception as e:
+            print(e)
+            return {}
+
+
+    def update(self, login, data):
+        try:
+            self._db_session.query(self.__model__).filter(self.__model__.email == login).update(data)
+            self._db_session.commit()
+            print("Пользователь обновлен")
+        except Exception as e:
+            print(e)
+            self._db_session.rollback()
--- project/tools/security.py
+++ project/tools/security.py
@@ -1,6 +1,8 @@
 import base64
 import hashlib
-
+import calendar
+import datetime
+import jwt
 from flask import current_app
 
 
@@ -16,4 +18,55 @@ def __generate_password_digest(password: str) -> bytes:
 def generate_password_hash(password: str) -> str:
     return base64.b64encode(__generate_password_digest(password)).decode('utf-8')
 
-# TODO: [security] Описать функцию compose_passwords(password_hash: Union[str, bytes], password: str)
+
+def compare_passwords(user_password, hashed_password):
+    return generate_password_hash(user_password) == hashed_password
+
+
+def generate_token(email, password_hash, password, is_refreshed=True):
+    if email is None:
+        return None
+
+    if not is_refreshed:
+        if not compare_passwords(password, password_hash):
+            return None
+
+    data = {
+        'email': email,
+        'password': password
+    }
+
+    # access_token
+    min15 = datetime.datetime.utcnow() + datetime.timedelta(minutes=current_app.config['TOKEN_EXPIRE_MINUTES'])
+    data['exp'] = calendar.timegm(min15.timetuple())
+    access_token = jwt.encode(data,
+                              key=current_app.config['SECRET_KEY'],
+                              algorithm=current_app.config['ALGORITHM'])
+
+    # refresh token
+    min_day = datetime.datetime.utcnow() + datetime.timedelta(minutes=current_app.config['TOKEN_EXPIRE_DAYS'])
+    data['exp'] = calendar.timegm(min_day.timetuple())
+    refresh_token = jwt.encode(data, key=current_app.config['SECRET_KEY'], algorithm=current_app.config['ALGORITHM'])
+
+    return {
+        'access_token': access_token,
+        'refresh_token': refresh_token
+    }
+
+
+def approve_token(token):
+    data = jwt.decode(token, key=current_app.config['SECRET_KEY'], algorithms=current_app.config['ALGORITHM'])
+
+    email = data.get('email')
+    password = data.get('password')
+
+    return generate_token(email=email, password_hash=None, password=password, is_refreshed=True)
+
+
+def get_data_from_token(refresh_token):
+    try:
+        data = jwt.decode(jwt=refresh_token, key=current_app.config['SECRET_KEY'],
+                          algorithms=[current_app.config['ALGORITHM']])
+        return data
+    except Exception:
+        return None
